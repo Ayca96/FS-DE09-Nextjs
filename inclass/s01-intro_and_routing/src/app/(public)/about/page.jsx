f208d5c6817e70cd187a6c4a31693809d850229f
Ayca96 <aycakubra1@gmail.com>
2025-01-01 21:43:03 +0100

    first commit

---
+++ inclass/s01-intro_and_routing/src/app/(public)/about/page.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const metadata = {
+  title: "About Page",
+  description: "This is About Page",
+};
+
+const About = () => {
+  return (
+    <div className="text-center text-3xl">
+      <h1>About Page</h1>
+    </div>
+  );
+};
+
+export default About;
