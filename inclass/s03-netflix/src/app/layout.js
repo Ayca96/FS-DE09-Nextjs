0f061aed462d683e10ca84f31d42b4546e0f412f
Ayca96 <aycakubra1@gmail.com>
2025-01-02 10:35:08 +0100

    `feat: updated environment variables, login/register page, globals.css, layout.js and Navbar component`

--- inclass/s03-netflix/src/app/layout.js
+++ inclass/s03-netflix/src/app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import "react-toastify/dist/ReactToastify.css";
+import {ToastContainer} from "react-toastify"
+import Navbar from "@/components/Navbar";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -14,8 +16,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+          <Navbar/>
           {children}
-       
+          <ToastContainer/>
       </body>
     </html>
   );
--- inclass/s03-netflix/src/components/Navbar.jsx
+++ inclass/s03-netflix/src/components/Navbar.jsx
@@ -86,7 +86,7 @@ const Navbar = () => {
                   <MenuItems className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                     <MenuItem>
                       <Link
-                        href=" "
+                        href="/register"
                         className={classNames(
                           "bg-gray-100",
                           "block px-4 py-2 text-sm text-gray-700 hover:bg-slate-200 cursor-pointer"
@@ -97,7 +97,7 @@ const Navbar = () => {
                     </MenuItem>
                     <MenuItem>
                       <Link
-                        href=" "
+                        href="/login"
                         className={classNames(
                           "bg-gray-100",
                           "block px-4 py-2 text-sm text-gray-700 hover:bg-slate-200 cursor-pointer"
@@ -108,7 +108,7 @@ const Navbar = () => {
                     </MenuItem>
                     <MenuItem>
                       <Link
-                        href=" "
+                        href="/profile"
                         className={classNames(
                           "bg-gray-100",
                           "block px-4 py-2 text-sm text-gray-700 hover:bg-slate-200 cursor-pointer"
